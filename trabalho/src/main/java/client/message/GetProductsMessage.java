9f3a2a6b80c88826ab0109f2528a14c3bdc91081
Dantas198 <33515744+Dantas198@users.noreply.github.com>
2020-04-27 12:55:11 +0100

    commit da treta

--- trabalho/src/main/java/client/message/GetProductsMessage.java
+++ trabalho/src/main/java/client/message/GetProductsMessage.java
@@ -2,8 +2,6 @@ package client.message;
 
 import middleware.message.Message;
 
-import java.io.Serializable;
-
 public class GetProductsMessage extends Message {
     public GetProductsMessage() {
         super();
