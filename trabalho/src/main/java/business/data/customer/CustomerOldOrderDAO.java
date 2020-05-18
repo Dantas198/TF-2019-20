82730c20fba07638b4c6928a8ebab6a7c1aa45c4
Daniel Costa <daniel.costa1998@gmail.com>
2020-05-18 12:12:09 +0100

    Fix CustomerOldOrderDAO

--- trabalho/src/main/java/business/data/customer/CustomerOldOrderDAO.java
+++ trabalho/src/main/java/business/data/customer/CustomerOldOrderDAO.java
@@ -12,7 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerOldOrderDAO extends DAOSet<Order> {
-    public CustomerOldOrderDAO(Connection c, String customer_id) throws SQLException {
+    public CustomerOldOrderDAO(Connection c, String current_id, String current_order_id) throws SQLException {
         super(new DAOSetPS<>() {
             @Override
             public Order fromResultSet(ResultSet resultSet) throws SQLException {
@@ -22,15 +22,21 @@ public class CustomerOldOrderDAO extends DAOSet<Order> {
 
             @Override
             public PreparedStatement getAll() throws SQLException {
-                PreparedStatement ps = c.prepareStatement("SELECT * FROM \"order\" INNER JOIN \"customer\" ON \"order\".\"customer_id\" = \"customer\".\"id\" WHERE \"order\".\"customer_id\" = ? AND \"order\".\"id\" <> \"customer\".\"current_order_id\"");
-                ps.setString(1, customer_id);
+                String current_order_restrinction = current_order_id == null ? "" : " AND NOT \"id\" <> ?";
+                PreparedStatement ps = c.prepareStatement("SELECT * FROM \"order\" WHERE \"customer_id\" = ?" + current_order_restrinction);
+                ps.setString(1, current_id);
+                if(current_order_id != null)
+                    ps.setString(2, current_order_id);
                 return ps;
             }
 
             @Override
             public PreparedStatement size() throws SQLException {
-                PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM \"order\" INNER JOIN \"customer\" ON \"order\".\"customer_id\" = \"customer\".\"id\" WHERE \"order\".\"customer_id\" = ? AND \"order\".\"id\" <> \"customer\".\"current_order_id\"");
-                ps.setString(1, customer_id);
+                String current_order_restrinction = current_order_id == null ? "" : " AND NOT \"id\" <> ?";
+                PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM \"order\" WHERE \"customer_id\" = ?" + current_order_restrinction);
+                ps.setString(1, current_id);
+                if(current_order_id != null)
+                    ps.setString(2, current_order_id);
                 return ps;
             }
 
@@ -38,7 +44,7 @@ public class CustomerOldOrderDAO extends DAOSet<Order> {
             public PreparedStatement add(Order o) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("INSERT INTO \"order\" (\"id\", \"customer_id\") VALUES (?, ?)");
                 ps.setString(1, o.getId());
-                ps.setString(2, customer_id);
+                ps.setString(2, current_id);
                 return ps;
             }
 
@@ -56,22 +62,20 @@ public class CustomerOldOrderDAO extends DAOSet<Order> {
 
             @Override
             public PreparedStatement clear() throws SQLException {
-                PreparedStatement ps = c.prepareStatement("DELETE \"order\" FROM \"order\" INNER JOIN \"customer\" ON \"order\".\"customer_id\" = \"customer\".\"id\" WHERE \"order\".\"customer_id\" = ? AND \"order\".\"id\" <> \"customer\".\"current_order_id\"");
-                ps.setString(1, customer_id);
+                PreparedStatement ps = c.prepareStatement("DELETE FROM \"order\" WHERE \"customer_id\" = ?");
+                ps.setString(1, current_id);
                 return ps;
             }
 
             @Override
             public PreparedStatement empty() throws SQLException {
-                PreparedStatement ps = c.prepareStatement("SELECT 1 FROM \"order\" WHERE \"customer_id\" = ?");
-                ps.setString(1, customer_id);
-                return ps;
+                return null;
             }
 
             @Override
             public PreparedStatement contains(Object o) throws SQLException {
                 if(o instanceof Order) {
-                    PreparedStatement ps = c.prepareStatement("SELECT 1 FROM \"order\" INNER JOIN \"customer\" ON \"order\".\"customer_id\" = \"customer\".\"id\" WHERE \"order\".\"customer_id\" = ? AND \"order\".\"id\" <> \"customer\".\"current_order_id\"");
+                    PreparedStatement ps = c.prepareStatement("SELECT 1 FROM \"order\" WHERE \"id\" = ?");
                     ps.setString(1, ((Order) o).getId());
                     return ps;
                 } else {
